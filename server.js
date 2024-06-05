6c6de2caa8f776e3e680bdb5214c75e6b2a0cde5
pratapsatya007k <165646100+pratapsatya007k@users.noreply.github.com>
2024-06-05 23:46:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-const PORT = process.env.PORT || 279;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
